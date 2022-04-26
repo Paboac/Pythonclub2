17b258161e80cd742f79d855320aaa43626fe57f
Pablo Contreras <Pablo.Contreras@Seattlecolleges.edu>
2022-04-26 15:26:41 -0700

    Resource view commit

--- project/views.py
+++ project/views.py
@@ -1,5 +1,13 @@
+from urllib import request
 from django.shortcuts import render
+from .models import Resource, Event, Meeting, Members
+
 
 # Create your views here.
 def index(request): 
      return render(request, 'project/index.html')
+
+def resource(request):
+     resource_list=Resource.objects.all() 
+     return render(request, 'project/resource.html', {'resource_list': resource_list})
+
